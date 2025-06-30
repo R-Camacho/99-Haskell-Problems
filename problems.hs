a72fb135bad7992badc664455248b8ce1a7be96c
R-Camacho <rodrigompcamacho@gmail.com>
2025-06-30 18:45:13 +0100

    Solve problem 3

--- problems.hs
+++ problems.hs
@@ -10,3 +10,9 @@ myButLast [x, _] = x
 myButLast (_:xs) = myButLast xs
 myButLast _ = undefined
 
+-- Problem 3: Find the K'th element of a list.
+elementAt :: [a] -> Int -> a
+elementAt (x:_) 1 = x
+elementAt (_:xs) k = elementAt xs (k - 1)
+elementAt _ _ = undefined
+
