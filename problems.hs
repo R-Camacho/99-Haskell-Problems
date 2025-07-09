1f9c5d60b3192d291fd4929b61a2f498af0eadcb
R-Camacho <rodrigompcamacho@gmail.com>
2025-07-09 10:46:19 +0100

    Solve problem 22

--- problems.hs
+++ problems.hs
@@ -136,3 +136,10 @@ removeAt n xs
 -- Problem 21: Insert an element at a given position into a list.
 insertAt :: a -> [a] -> Int -> [a]
 insertAt x xs n = take (n - 1) xs ++ [x] ++ drop (n - 1) xs
+
+-- Problem 22: Create a list containing all integers within a given range.
+range :: Int -> Int -> [Int]
+range x n
+    | x >= n = [x] 
+    | otherwise = x : range (x + 1) n
+
