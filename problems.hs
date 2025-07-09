6e1c04f307f20ddb9d6f9608c5871b1267165f03
R-Camacho <rodrigompcamacho@gmail.com>
2025-07-09 10:25:10 +0100

    Solve problem 21

--- problems.hs
+++ problems.hs
@@ -133,3 +133,6 @@ removeAt n xs
     | n >= 1 && n <= length xs = (xs !! (n - 1), take (n - 1) xs ++ drop n xs) 
     | otherwise                = undefined
 
+-- Problem 21: Insert an element at a given position into a list.
+insertAt :: a -> [a] -> Int -> [a]
+insertAt x xs n = take (n - 1) xs ++ [x] ++ drop (n - 1) xs
